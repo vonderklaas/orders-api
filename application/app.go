c9694af139cdee8def61dcb25cc4e869302693ae
Nick Garbalau <garbalaunick@gmail.com>
2024-04-23 12:24:44 +0300

    fix: add redis and graceful shutdown

--- application/app.go
+++ application/app.go
@@ -4,15 +4,20 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/redis/go-redis/v9"
 )
 
 type App struct {
 	router http.Handler
+	rdb    *redis.Client
 }
 
 func New() *App {
 	app := &App{
 		router: loadRoutes(),
+		rdb:    redis.NewClient(&redis.Options{}),
 	}
 
 	return app
@@ -25,10 +30,41 @@ func (a *App) Start(ctx context.Context) error {
 		Handler: a.router,
 	}
 
-	err := server.ListenAndServe()
-
+	err := a.rdb.Ping(ctx).Err()
 	if err != nil {
-		return fmt.Errorf("failed to start server: %w", err)
+		return fmt.Errorf("failed to connect to redis: %w", err)
+	}
+
+	defer func() {
+		if err := a.rdb.Close(); err != nil {
+			fmt.Println("failed to close redis", err)
+		}
+	}()
+
+	fmt.Println("> starting server")
+
+	// Make channel to send info back to main thread
+	ch := make(chan error, 1)
+
+	// Run server concurrently
+	go func() {
+		err = server.ListenAndServe()
+		if err != nil {
+			// Send arrow to anyone who is listening to this buffer channel
+			ch <- fmt.Errorf("failed to start server: %w", err)
+		}
+		// Channel is done, so inform reader to stop expecting data
+		close(ch)
+	}()
+
+	select {
+	case err = <-ch:
+		return err
+	case <-ctx.Done():
+		timeout, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+
+		return server.Shutdown(timeout)
 	}
 
 	// Return `nil` for success case
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 
 	"github.com/vonderklaas/orders-api/application"
 )
@@ -11,8 +13,13 @@ func main() {
 
 	app := application.New()
 
-	err := app.Start(context.TODO())
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	// Call at the end of main function
+	defer cancel()
+
+	err := app.Start(ctx)
 	if err != nil {
 		fmt.Println("failed to start app:", err)
 	}
+
 }
